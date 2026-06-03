fcb7bf37a998298a7ab06ff3f531a500e08e8036
Lolina lc <lolinalopez1192@gmail.com>
2026-06-03 00:38:45 -0600

    feat: add JS entry point main.js

---
+++ js/main.js
@@ -0,0 +1,7 @@
+import { init as initNav } from "./modules/nav.js";
+import { init as initFaq } from "./modules/faq.js";
+import { init as initAnimations } from "./modules/animations.js";
+
+initNav();
+initFaq();
+initAnimations();
